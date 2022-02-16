c2b8fe36a36027ec0234b1395835352a5d655625
wangbo233 <13379304436@163.com>
2022-02-17 00:02:17 +0800

    validation of username and email

--- webserver/form.py
+++ webserver/form.py
@@ -1,6 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,DateField,SelectField
-from wtforms.validators import DataRequired,Email,EqualTo,Length
+from wtforms.validators import DataRequired,Email,EqualTo,Length,ValidationError
+from webserver.models import User,Post
+
+
 
 class LoginForm(FlaskForm):
     email = StringField('email',validators=[DataRequired(),Email()])
@@ -11,4 +14,15 @@ class RegisterForm(FlaskForm):
     email = StringField('email',validators=[DataRequired(),Email()])
     password = PasswordField('password',validators=[DataRequired()])
     password_confirmation = PasswordField('password_confirmaiton',validators=[DataRequired(),EqualTo('password')])
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username = username.data).first()
+        if user:
+            raise ValidationError("该用户名已经被注册！请更换用户名！")
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email = email.data).first()
+        if user:
+            raise ValidationError("该邮箱已经被注册！请使用别的邮箱！")
+
 
