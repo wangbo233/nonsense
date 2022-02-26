e1cff45550ae78c8df285bc719fa47aacc0a839d
wangbo233 <13379304436@163.com>
2022-02-27 01:50:32 +0800

    page_cache

--- process_queue.py
+++ process_queue.py
@@ -9,5 +9,6 @@ if __name__ == '__main__':
     conn = Redis.from_url(REDIS_URL)
     # 这里的上下文一直使用localhost, 需啊在邮件传递时指明request.root_url
     with app.app_context(), app.test_request_context():
+        # 使用redis队列发送邮件
         process_send_reset_email_via_queue(conn)
 
--- webserver/main/routes.py
+++ webserver/main/routes.py
@@ -1,8 +1,14 @@
-from flask import  Blueprint,render_template, url_for, request
+from flask import Blueprint, render_template,  request, current_app, url_for
 
 main = Blueprint('main',__name__)
 
+
 @main.route('/')
 def index():
-    print(request.url)
-    return render_template("index.html")
+    conn = current_app.redis
+    page_key = "page:cache"+str(hash(url_for('main.index')))
+    content = conn.get(page_key)
+    if not content:
+        content = render_template("index.html")
+        conn.setex(page_key, 300, content)
+    return content
